a6f72313d59595d084a02e0f3c7b730cb3331186
Sceat <mr.sceat@outlook.com>
2016-11-26 08:40:59 +0100

    WIP fight behavior

--- src/main/java/fr/aresrpg/eratz/domain/behavior/fight/FightBehavior.java
+++ src/main/java/fr/aresrpg/eratz/domain/behavior/fight/FightBehavior.java
@@ -3,6 +3,7 @@ package fr.aresrpg.eratz.domain.behavior.fight;
 import fr.aresrpg.eratz.domain.behavior.Behavior;
 import fr.aresrpg.eratz.domain.dofus.mob.Mob;
 import fr.aresrpg.eratz.domain.dofus.player.Spell;
+import fr.aresrpg.eratz.domain.dofus.player.Spells;
 import fr.aresrpg.eratz.domain.player.Player;
 
 import java.util.Set;
@@ -14,33 +15,43 @@ import java.util.Set;
 public interface FightBehavior extends Behavior {
 
 	boolean canLaunchSpell(Spell s);
-	
+
 	boolean canEndTurnHere(int cellid);
-	
-	int getTryToReachCellId(); // dans le cas ou canEndTurnHere = false (par exemple pour le chall hardi ou il faut coller un  mob)
-	
+
+	int getTryToReachCellId(); // dans le cas ou canEndTurnHere = false (par exemple pour le chall hardi ou il faut coller un mob)
+
 	Mob getTarget();
-	
+
+	Mob getNearestMob();
+
 	Spell getSpellNeededForChallenge();
-	
+
 	Set<Spell> getBuffSpells();
-	
+
 	Set<Spell> getOffensiveSpells();
-	
-	boolean canAttackTarget(Spell spell,Mob target);
-	
+
+	Set<Player> getAllies();
+
+	boolean canAttackTarget(Spell spell, Mob target);
+
 	boolean canUseMagicalSpell();
-	
+
 	boolean canUseWeapon();
-	
+
 	boolean canUseTrapAndGlyphes();
-	
+
 	boolean needToKillAnEnnemiBeforeHisTurn();
-	
+
 	boolean needToKillAtLeastOneMob();
-	
+
 	boolean canTakeDamage();
-	
+
 	boolean needToKillAnAlly(Player p);
-	
+
+	int getTotalMobKilled();
+
+	int getMobKilledInTurn();
+
+	boolean hasAlreadyLaunched(Spells s);
+
 }
