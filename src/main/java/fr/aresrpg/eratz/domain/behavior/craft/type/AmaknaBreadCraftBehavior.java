10c2b4a1fdb924974a43f939e3bd80e20ecb44f1
Sceat <mr.sceat@outlook.com>
2016-12-05 23:02:30 +0100

    Finish amaknabread craft

--- src/main/java/fr/aresrpg/eratz/domain/behavior/craft/type/AmaknaBreadCraftBehavior.java
+++ src/main/java/fr/aresrpg/eratz/domain/behavior/craft/type/AmaknaBreadCraftBehavior.java
@@ -8,9 +8,12 @@
  *******************************************************************************/
 package fr.aresrpg.eratz.domain.behavior.craft.type;
 
+import fr.aresrpg.eratz.domain.ability.BaseAbility;
+import fr.aresrpg.eratz.domain.ability.craft.CraftAbility;
+import fr.aresrpg.eratz.domain.ability.move.Navigation;
 import fr.aresrpg.eratz.domain.behavior.craft.CraftBehavior;
 import fr.aresrpg.eratz.domain.dofus.item.Items;
-import fr.aresrpg.eratz.domain.dofus.item.Recipe;
+import fr.aresrpg.eratz.domain.dofus.map.Zaapi;
 import fr.aresrpg.eratz.domain.player.Perso;
 import fr.aresrpg.eratz.domain.provider.type.WaterProvider;
 
@@ -20,14 +23,11 @@ import fr.aresrpg.eratz.domain.provider.type.WaterProvider;
  */
 public class AmaknaBreadCraftBehavior extends CraftBehavior {
 
-	private WaterProvider waterProvider;
-
 	/**
 	 * @param perso
 	 */
 	public AmaknaBreadCraftBehavior(Perso perso, int craft) {
 		super(perso, craft);
-		this.waterProvider = new WaterProvider(perso, craft);
 
 	}
 
@@ -38,7 +38,34 @@ public class AmaknaBreadCraftBehavior extends CraftBehavior {
 
 	@Override
 	public void start() {
-		Recipe recipe = getItemToCraft().getRecipe();
-
+		if (getPerso().getQuantityInInventoryOf(Items.FARINE_DE_BLE.getId()) > 0) getPerso().goEmptyInvInBanque(492, 8540, 577);
+		int farineNeed = getQuantity() - getPerso().getQuantityInBanqueOf(Items.FARINE_DE_BLE.getId());
+		int waterNeed = getQuantity() - (getPerso().getQuantityInInventoryOf(Items.EAU.getId()) + getPerso().getQuantityInBanqueOf(Items.EAU.getId()));
+		if (waterNeed > 0) new WaterProvider(getPerso(), waterNeed).provide();
+		getPerso().goEmptyInvInBanque(492, 8540, 577);
+		if (farineNeed > 0)
+			new WheatFlourCraftBehavior(getPerso(), farineNeed).start();
+		BaseAbility ba = getPerso().getBaseAbility();
+		Navigation na = getPerso().getNavigation();
+		CraftAbility ca = getPerso().getCraftAbility();
+		int tocraft = getQuantity();
+		int eauId = Items.EAU.getId();
+		int farineId = Items.FARINE_DE_BLE.getId();
+		while (tocraft > 0) {
+			ba.goAndOpenBank();
+			int totalpodForOne = Items.EAU.getPod() + Items.FARINE_DE_BLE.getPod();
+			int eau = getPerso().getFreePods() / totalpodForOne;
+			int farine = getPerso().getFreePods() - eau;
+			ba.getItemFromBank(eauId, eau);
+			ba.getItemFromBank(farineId, farine);
+			ba.goToZaapi(Zaapi.BONTA_HDV_BOULANGER);
+			na.moveToCell(472).moveToCell(219);
+			ba.useCraftingMachine(2);
+			ca.placeItem(eauId, 1);
+			ca.placeItem(farineId, 1);
+			ca.startCraft(tocraft);
+			tocraft -= getPerso().getQuantityInInventoryOf(Items.PAIN_AMAKNA.getId());
+			getPerso().goEmptyInvInBanque(492, 8540, 577);
+		}
 	}
 }
